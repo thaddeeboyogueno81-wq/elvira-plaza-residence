60135b130f81b31c55cd5aa1a420aef23af1b846
thaddeeboyogueno81-wq <thaddeeboyogueno81@gmail.com>
2026-09-24 08:20:31 +0100

    chore: remove legacy server.js du projet MERN

--- server.js
+++
@@ -1,7 +0,0 @@
-const express = require("express");
-require('dotenv').config({path: './config/.env'});
-const app = express();
-
-app.listen(process.env.PORT, ()=>{
-    console.log(`Serveur branché sur le port ${process.env.PORT}`);
-});
